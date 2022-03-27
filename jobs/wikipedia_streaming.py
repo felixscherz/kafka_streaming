4420913060357eb3f5a650a32536862b7148f683
Felix <felixwscherz@gmail.com>
2022-03-27 15:35:33 +0200

    setup streaming jobs to get edits by the minute

--- jobs/wikipedia_streaming.py
+++ jobs/wikipedia_streaming.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
 
 import sys
 from random import random
@@ -18,6 +20,36 @@ if __name__ == '__main__':
     #     .option("password", "password")\
     #     .option('driver', 'org.postgresql.Driver')\
     #     .load()
+    spark.sparkContext.setLogLevel("ERROR")
+    jsonschema = StructType().add("$schema", StringType())\
+            .add("id", StringType())\
+            .add("namespace", StringType())\
+            .add("title", StringType())\
+            .add("comment", StringType())\
+            .add("timestamp", TimestampType())\
+            .add("user", StringType())\
+            .add("bot", BooleanType())\
+            .add("minor", BooleanType())\
+            .add("patrolled", BooleanType())\
+            .add("server_url", StringType())\
+            .add("server_name", StringType())\
+            .add("server_script_path", StringType())\
+            .add("wiki", StringType())\
+            .add("parsedcomment", StringType())\
+            .add("meta_domain", StringType())\
+            .add("meta_uri", StringType())\
+            .add("meta_request_id", StringType())\
+            .add("meta_stream", StringType())\
+            .add("meta_topic", StringType())\
+            .add("meta_dt", DateType())\
+            .add("meta_partition", IntegerType())\
+            .add("meta_offset", IntegerType())\
+            .add("meta_id", StringType())\
+            .add("length_old", IntegerType())\
+            .add("length_new", IntegerType())\
+            .add("revision_old", IntegerType())\
+            .add("revision_new", IntegerType())
+
     print('initializing streaming...')
     df = spark \
       .readStream \
@@ -27,10 +59,27 @@ if __name__ == '__main__':
       .load()
 
     print('connected to kafka')
-    df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+    parsed = df.select(
+        from_json(col("value").cast("string"),
+                  jsonschema).alias("parsed_value"))\
+                          .select("parsed_value.*")
     print('select expression')
-    query = df.writeStream\
+    parsed.printSchema()
+
+    # get global changes by the minute
+    global_edits = parsed\
+            .groupBy(window("timestamp", "1 minute"))\
+            .count()
+
+    # for german wikipedia filter by server_name
+    german_edits = parsed\
+            .filter(parsed.server_name.rlike(r"de\..*"))\
+            .groupBy(window("timestamp", "1 minute"))\
+            .count()
+
+    query = german_edits.writeStream\
            .format("console")\
+           .outputMode("complete")\
            .start()
 
     query.awaitTermination()
